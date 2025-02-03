c1c54dc60264bd5fb643ae8bbb482519d2a4804d
4dblackhole <4dblackhole@naver.com>
2025-02-04 02:56:56 +0900

    초기 커밋.

---
+++ DX12test/libraries.h
@@ -0,0 +1,34 @@
+#pragma once
+
+//Windows
+#include <Windows.h>
+
+
+//STL
+#include <algorithm>
+#include <array>
+#include <assert.h>
+#include <chrono>
+#include <codecvt>
+#include <fstream>
+#include <functional>
+#include <malloc.h>
+#include <map>
+#include <unordered_map>
+#include <memory.h>
+#include <mutex>
+#include <set>
+#include <span>
+#include <sstream>
+#include <stdlib.h>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <tchar.h>
+#include <thread>
+#include <queue>
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
---
+++ DX12test/main.cpp
@@ -0,0 +1,11 @@
+#include "framework.h"
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPTSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+
+	MessageBox(NULL, _T("asdf"), _T("asdf"), MB_OK);
+	return 0;
+}
